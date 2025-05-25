fdfa161397a6ed3c13178c293d4ee5e0e5338203
Antoine Vignes <a.vignes@mcapital.fr>
2025-05-25 12:59:03 +0200

    sign in ok

---
+++ src/auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Google],
+  callbacks: {
+    authorized: async ({ auth }) => {
+      return !!auth;
+    },
+  },
+
+  pages: {
+    signIn: "/login",
+  },
+});
